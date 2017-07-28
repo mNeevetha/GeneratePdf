805e5c4b6c10f0b50fdeb0e793732e42dd67133d
mNeevetha <neevetha.m@cognizant.com>
2017-07-28 12:51:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
 });
