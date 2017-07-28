2702b0581496ee84ff62d41741fa732eb9cc5d63
mNeevetha <neevetha.m@cognizant.com>
2017-07-28 12:39:52 +0530

    Update apppdf.js

--- apppdf.js
+++ apppdf.js
@@ -5,7 +5,7 @@ const router = express.Router();
 var pdf = require('html-pdf');
 var contentbody;
 // Fetch List of APS query records for a single user
-router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
  
  res.send('Hello PDF!');
  
