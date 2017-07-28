7d7b17c363fca15abb7b9ea4dd1b39f5ed0aba1d
Neevetha <neevetha.m@cognizant.com>
2017-07-28 17:45:07 +0530

    added code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 const aps = require('./API');
 const apppdf = require('./apppdf');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -19,11 +20,17 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+//Pdf download location
+//app.use('/APSDownloads', express.static('APSDownloads'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/APSDownloads')));
+
 app.use(bodyParser.json());
 app.use('/api', aps);
 app.use('/pdf', apppdf);
 // Listen
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Listening on localhost:'+ port);
 
@@ -40,5 +47,9 @@ app.use((err, req, res, next) => {
     error: err,
   });
 });
+
+app.get('/APSDownloads', function (req, res) {
+  res.sendFile(path.join(__dirname, '/APSDownloads/', 'APSReport1501152671998.pdf'));
+})
 
 module.exports = app;
--- apppdf.js
+++ apppdf.js
@@ -1,38 +1,64 @@
 const express = require('express');
 const router = express.Router();
-//const fetch=require('isomorphic-fetch');
-//var fs = require('fs');
 var pdf = require('html-pdf');
+var fs = require('fs');
 var contentbody;
 // Fetch List of APS query records for a single user
 router.post('/', (req, res, next) => {
- 
-filePath ="./APSDownloads/APSReport"+new Date().getTime()+".pdf";
+  console.log("inside post")
 
-var html = req.body.htmlPage;
-  var options = { format: 'Letter',"border": {
-    "top": "0mm",            // default is 0, units: mm, cm, in, px 
-    "right": "0mm",
-    "bottom": "0mm",
-    "left": "5mm"
-  }, };
+  filePath = "./APSDownloads/APSReport" + new Date().getTime() + ".pdf";
+  var message;
+
+  var html = req.body.htmlPage;
+  var options = {
+    format: 'Letter', "border": {
+      "top": "0mm",            // default is 0, units: mm, cm, in, px 
+      "right": "0mm",
+      "bottom": "0mm",
+      "left": "5mm"
+    },
+  };
   //const decodedHtmlPage = urlencode.decode(html);
 
-  
-    pdf
-      .create(html, options)
-      .toFile(filePath, function(err, res) {
-       
-        if (err) {
-          res.json({ message: 'Print error' , link: filePath, erro:err,res:res });
-        }else
-        {
-        res.json({ message: 'Print Sucess' , link: filePath, erro:err,res:res });
-        }
-      });
-    //res.json({ message: 'Print sucess', link: filePath});
-  
 
+  // pdf
+  //   .create(html, options)
+  //   .toFile(filePath, function (err, res) {
+  //     console.log(res);
+  //     if (err) {
+  //       message = "Download Failed";
+  //     } else {
+  //       message = "Download Success";
+  //     }
+  //   });
+
+  pdf.create(html).toStream(function (err, stream) {
+    // console.log("streamVall");
+    // res.write("stream")
+    // res.end();
+    //  var streamVall= stream.pipe(fs.createWriteStream('./APSDownloads/foo.pdf'));
+    // console.log(streamVall);
+
+    var chunks = [];
+    stream.on('data', function (chunk) {
+      chunks.push(chunk);
+      console.log('chunk:', chunk.length);
+    });
+    stream.on('end', function () {
+      var result = Buffer.concat(chunks);
+      console.log('final result:', result.toString('base64'));
+
+      res.contentType("application/pdf");
+      res.setHeader('Content-disposition', 'attachment;filename=test.pdf');
+      res.setHeader('Content-Length', result.toString('base64').length);
+
+      res.send(result.toString('base64'));
+    });
+  });
+
+
+  //res.json({ message: message });
 });
 
 module.exports = router;
